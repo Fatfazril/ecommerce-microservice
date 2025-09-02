e06e825108ccefb36d86764ecbb6dc2d701d4f2f
Azril Fatfa <azril.fatfa@gmail.com>
2025-09-02 11:24:45 +0700

    Remove auth-service from product branch

--- auth-service/src/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.connectDB = async function(uri){
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  console.log('[auth] Mongo connected');
-};
